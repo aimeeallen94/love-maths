23109310dfe8d7dc88854bf1aa1db0c5c2f9aa1c
aimeeallen94 <>
2023-06-23 18:41:53 +0000

    fixing addition question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,41 @@
-//* Wait for the DOM to finish loading before running the game
-//* Get the button elements and add listeners to these
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add listeners to them
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if(this.getAttribute("data-type") === "submit") {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
-            } else { 
+            } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
             }
-        })
+        });
     }
 
-    runGame('addition');
+    runGame("addition");
 
 });
 
-/** The main game "loop", called when the script is first loaded 
+/** 
+ * The main game "loop", called when the script is first loaded 
  * and after the user's answer has been processed
 */
 
 function runGame(gameType) {
 
-    //* Creates two random numbers between 1 and 25
-    let num1 = math.floor(math.random() * 25) + 1;
-    let num2 = math.floor(math.random() * 25) + 1;
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
-    if (gameType === 'addition') {
+    if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
     } else {
-        alert(`Unknown game type: ${}`);
-        throw `Unknown game type: ${}. Aborting!`;
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
     }
-    }
-
 }
 
 function checkAnswer() {
@@ -59,7 +58,7 @@ function displayAdditionQuestion(operand1, operand2) {
 
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = '+';
+    document.getElementById('operator').textContent = "+";
 
 }
 
